e255ac6fc2c2bf8ca43a711980be84dcc9d22e32
nafis <user@Napiss-2.local>
2025-11-08 11:15:47 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="text-center">
+    <h1 class="text-4xl font-bold text-blue-700 mb-4">Selamat Datang</h1>
+    <p class="text-gray-600">Ini adalah halaman utama aplikasi kamu.</p>
+</div>
+@endsection
